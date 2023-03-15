bd3f2e1e41123ce076e55c317267d8d3895b95f5
myk6 <myk6@fresnou.org>
2023-03-15 14:18:49 -0700

    yes

---
+++ Functions1.js
@@ -0,0 +1,21 @@
+// function findAreaOfRectangle(length, width){
+//     console.log(length * width)
+// }
+// findAreaOfRectangle( 2,  3)
+
+
+// function findAreaOfCircle(radius){
+//     console.log(radius *radius*3.14)
+// }
+// findAreaOfCircle(4);
+
+
+function findMin(x, y){
+    if (x>y){
+        console.log(`the larger the number is ${x}`);
+    }else {
+        console.log('the larger the number is ${y');
+    }
+}
+
+printMax(x=8,y=21);
