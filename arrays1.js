e3fd132fe82b44949e63e1e0f7034feef4489ab4
myk6 <myk6@fresnou.org>
2023-03-24 14:19:09 -0700

    saved arrays

---
+++ arrays1.js
@@ -0,0 +1,38 @@
+// function firstElement(arr){
+//     console.log(arr[0]);
+// }
+// let myNums=[1,2,3,4];
+// firstElement(myNums);
+
+
+
+
+
+// function lastElement(arr){
+//     console.log(arr[3]);
+// }
+// let nums=[3,4,5,6];
+// lastElement(nums);
+
+
+
+
+
+// function printSecond(arr){
+//     if(arr.length == 0){
+//         console.log("Empty array");
+//     } else{
+//         console.log( arr[0] )
+//     }
+// }
+// let Numb = [10, 20, 30, 40];
+// //firstElement (Numb);
+// printSecond(Numb) ;
+//
+// let empty = [];
+// printSecond(empty) ;
+
+
+
+
+
